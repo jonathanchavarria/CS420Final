1474e4380166cf7e24eb51d1e4e29dfde85fd742
jonathanchavarria <jchavarria@nevada.unr.edu>
2022-05-04 22:15:18 -0700

    added homepage and stuff

--- lib/chirp_web/router.ex
+++ lib/chirp_web/router.ex
@@ -27,10 +27,13 @@ defmodule ChirpWeb.Router do
 
     live "/users", UserLive.Index, :index
     live "/users/new", UserLive.Index, :new
+    live "/users/home", UserLive.Home, :index
     live "/users/:id/edit", UserLive.Index, :edit
 
     live "/users/:id", UserLive.Show, :show
     live "/users/:id/show/edit", UserLive.Show, :edit
+
+
   end
 
   # Other scopes may use custom stacks.
