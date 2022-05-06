da9b6d193a5a09607fab906d2590dd16bdb9639f
Jeffrey Carson <carsonwing4@gmail.com>
2022-05-05 22:47:02 -0700

    new

--- lib/chirp_web/live/user_live/about.ex
+++ lib/chirp_web/live/user_live/about.ex
@@ -4,7 +4,14 @@ defmodule ChirpWeb.PageLive.Home do
   def render(assigns) do
     ~H"""
 
-    bruh momemt
+    <div id = "boxjeff">
+        <div class = "aboutTitlejeff">
+            <h1 class = "wordsjeff">ABOUT</h1>
+        </div>
+        <div class = "textboxjeff">
+            <p class = "wordsjeff">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti vitae eligendi quia ipsum voluptatibus fugiat ut sint veritatis repellendus quasi. Fugiat, dolor, fugit maiores vero nostrum earum exercitationem, ut animi dolore vel totam? Facere, voluptatum quaerat. Ratione inventore dolorum culpa mollitia rerum. Accusantium, mollitia. Ipsum facilis a enim tempore molestiae.</p>
+        </div>
+    </div>
 
 
     """
