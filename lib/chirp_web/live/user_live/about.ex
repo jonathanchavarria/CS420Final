63130b03703b6e32c9622568ffff85188db1e1da
Jeffrey Carson <carsonwing4@gmail.com>
2022-05-05 22:48:24 -0700

    testing

--- lib/chirp_web/live/user_live/about.ex
+++ lib/chirp_web/live/user_live/about.ex
@@ -9,7 +9,7 @@ defmodule ChirpWeb.PageLive.Home do
             <h1 class = "wordsjeff">ABOUT</h1>
         </div>
         <div class = "textboxjeff">
-            <p class = "wordsjeff">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti vitae eligendi quia ipsum voluptatibus fugiat ut sint veritatis repellendus quasi. Fugiat, dolor, fugit maiores vero nostrum earum exercitationem, ut animi dolore vel totam? Facere, voluptatum quaerat. Ratione inventore dolorum culpa mollitia rerum. Accusantium, mollitia. Ipsum facilis a enim tempore molestiae.</p>
+            <p class = "wordsjeff">Lorem ipsumdfd dolor sit, amet consectetur adipisicing elit. Deleniti vitae eligendi quia ipsum voluptatibus fugiat ut sint veritatis repellendus quasi. Fugiat, dolor, fugit maiores vero nostrum earum exercitationem, ut animi dolore vel totam? Facere, voluptatum quaerat. Ratione inventore dolorum culpa mollitia rerum. Accusantium, mollitia. Ipsum facilis a enim tempore molestiae.</p>
         </div>
     </div>
 
