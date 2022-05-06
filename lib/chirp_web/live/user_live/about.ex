2a4729565bee4fd7bd092e5ea84c43f4bf543e81
Jeffrey Carson <carsonwing4@gmail.com>
2022-05-06 00:41:36 -0700

    fixes

--- lib/chirp_web/live/user_live/about.ex
+++ lib/chirp_web/live/user_live/about.ex
@@ -10,21 +10,21 @@ defmodule ChirpWeb.PageLive.Home do
         </div>
         <div class = "textboxjeff">
             <p class = "wordsjeff">Vibe Has <i>RELATIONSHIPS...</i></p>
-            <p class = "info">Users Can:</p>
-            <ul class = "info">
+            <p class = "infoj">Users Can:</p>
+            <ul class = "infoj">
               <li>- Make New Connections</li>
               <li>- Visualize Social Circle</li>
               <li>- Post Images and Express Oneself</li>
             </ul>
-            <p class = "info">Getting Started:</p>
-            <ul class = "info">
+            <p class = "infoj">Getting Started:</p>
+            <ul class = "infoj">
               <li>- Click Sign Up</li>
               <li>- Enter Your Desired Name, Username, Bio, and Photos</li>
               <li>- Congrats, your account is made!!</li>
               <li>- Continue to explore the Home Page and Friend Map!</li>
             </ul>
-            <p class = "info">QUESTIONS?</p>
-            <ul class = "info">
+            <p class = "infoj">QUESTIONS?</p>
+            <ul class = "infoj">
               <li>Email jeffreycarson@nevada.unr.edu or</li>
               <li>jchavarria@nevada.unr.edu for help!</li>
             </ul>
